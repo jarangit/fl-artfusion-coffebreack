f253172a4dde7baa7ab879b79890092aa1d617df
jarangit <jaran.dch@gmail.com>
2023-08-28 23:33:45 +0700

    improve: logo

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -7,14 +7,20 @@ type Props = {}
 const Menu = (props: Props) => {
   return (
     <div className='flex justify-between my-container items-center  py-6'>
-      <div>
-        <a href={'/'}>
-          <Image
-            src={'/img/logo.png'}
-            alt=''
-            width={200}
-            height={200}
-          />
+      <div className=''>
+        <a href={'/'} className='flex  gap-1  justify-center'>
+          <div>
+            <Image
+              src={'/img/coffee.png'}
+              alt=''
+              width={40}
+              height={40}
+            />
+          </div>
+          <div>
+            <div className='font-bold text-3xl text-logo'>CoffeeBreak</div>
+            <div className='text-2xl font-semibold text-[#A88C6A] relative -top-3'>loans<span className='text-gray font-medium text-xl text-[#D7D7D7]'>.com</span></div>
+          </div>
         </a>
       </div>
       <div className='hidden gap-10 text-xl md:flex '>
